c52324b5bbc2f6247bc30c6abd61ab2025105130
aburidho <ridhoabu127@gmail.com>
2022-02-02 11:02:39 +0700

    yo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Button from "./BtnItems";
 
 export default function App() {
     return (
-    <nav className="navbar bg-blue-600 py-12">
+    <nav className="navbar bg-indigo-600 py-12">
       <div className="container flex mx-auto">
         <div className="w-3/12">
           <Logo/>
@@ -17,13 +17,13 @@ export default function App() {
         </div>
       </div>
       <div>
-      <div className="text-5xl my-12 mt-20 font-mono text-white text-center">Saya seorang front-end engineer, back-end <br/>engineer, dan juga UI designer</div>
+      <div className="text-5xl my-12 mt-20 font-mono text-white text-center leading-relaxed">Saya seorang front-end engineer, back-end <br/>engineer, dan juga UI designer</div>
       <div className="mt-12 text-2xl text-white text-center opacity-50">Sejak umur 17 tahun saya memulai programming. <br/> Dan baru-baru ini memulai UI designer.</div>
       </div>
-      <div className="text-center mt-10">
+      <div className="text-center mt-12">
           <a href="#" className="border-yellow-400 bg-yellow-400 rounded-full border-2 hover:bg-yellow-400 hover:border-yellow-400 px-8 py-2 text-black font-semibold text-xl">Klik!</a>
       </div>
       <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     </nav>
     )
-}
+}       
